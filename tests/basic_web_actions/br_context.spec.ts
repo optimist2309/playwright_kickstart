4db4204e5203b3e301286a1a0bfff26794b23b49
optimist2309 <harishtiwary46@gmail.com>
2025-12-08 23:21:28 +0530

    Context stuff-1

---
+++ tests/basic_web_actions/br_context.spec.ts
@@ -0,0 +1,42 @@
+import {test,  expect,Page, chromium , firefox} from '@playwright/test';
+
+// Browser >>>>> context >>> page
+
+// Brwser ----> chromoium , firefox , webkit
+
+// Context ---->> user profile[Multiple context can be created in a browser]
+
+// page ---->> new tab , window, popup
+
+test('Browser context stuff', async ({ context }) => {
+const page  = await context.newPage();
+await  page.goto("https://testautomationpractice.blogspot.com/") ;
+});
+
+test('Another Browser context stuff', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page  = await context.newPage();
+    await page.goto("https://testautomationpractice.blogspot.com/") ;
+    });
+
+    test('One more Browser context stuff', async ({  }) => {
+    const browser  = await chromium.launch();
+    const context = await browser.newContext();
+    const page  = await context.newPage();
+    await  page.goto("https://testautomationpractice.blogspot.com/") ;
+
+});
+
+test('One more Browser context stuff-3', async ({  }) => {
+    const browser  = await firefox.launch();
+    const context = await browser.newContext();
+    const page  = await context.newPage();
+    await  page.goto("https://testautomationpractice.blogspot.com/") ;
+
+});
+
+test('One more Browser context stuff-2', async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/") ;
+    await page.waitForTimeout(5000)
+});
+
