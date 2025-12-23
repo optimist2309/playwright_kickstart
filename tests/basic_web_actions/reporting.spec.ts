965872d1b51dc5ebbd200a1bc67e6b9390be0c84
optimist2309 <harishtiwary46@gmail.com>
2025-12-23 09:10:51 +0530

    reporting stuff

--- tests/basic_web_actions/reporting.spec.ts
+++ tests/basic_web_actions/reporting.spec.ts
@@ -51,4 +51,25 @@ Or pass the same values via the command line:
 
 npx playwright test --reporter=line,allure-playwright
 
+
+Dot reporter
+Dot reporter is very concise - it only produces a single character per successful test run. It is the default on CI and useful where you don't want a lot of output.
+
+npx playwright test --reporter=dot
+
+playwright.config.ts
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  reporter: 'dot',
+});
+
+Blob reporter
+Blob reports contain all the details about the test run and can be used later to produce any other report. Their primary function is to facilitate the merging of reports from sharded tests.
+
+npx playwright test --reporter=blob
+
+By default, the report is written into the blob-report directory in the package.json directory or current working directory (if no package.json is found).
+
+The report file name looks like report-<hash>.zip or report-<hash>-<shard_number>.zip when sharding is used. The hash is an optional value computed from --grep, --grepInverted, --project, testConfig.tag and file filters passed as command line arguments. The hash guarantees that running Playwright with different command line options will produce different but stable between runs report names. The output file name can be overridden in the configuration file or passed as 'PLAYWRIGHT_BLOB_OUTPUT_FILE' environment variable.
 */
